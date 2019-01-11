258f9e0b94f5d806b3616f7cc326cff3056e86d5
SmallBear95 <44842177+SmallBear95@users.noreply.github.com>
2019-01-11 14:24:37 +0200

    upasdfasdgfasdgfawergt

---
+++ app/src/main/java/com/example/aviator/mercury_new/Sign_UpINFO.java
@@ -0,0 +1,13 @@
+package com.example.aviator.mercury_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Sign_UpINFO extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign__up_info);
+    }
+}
